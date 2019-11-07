d4f0cefa02317c987ed9f5c1e60a1ebdbb8a433a
Kevin_ROH <kwangmin.roh@gmail.com>
2019-11-07 09:49:53 +0900

    merge origin/master with Github

--- chapter007/HelloWorld/HelloWorld/Program.cs
+++ chapter007/HelloWorld/HelloWorld/Program.cs
@@ -16,7 +16,7 @@ namespace HelloWorld
             Console.WriteLine("master : 2nd Hello World");
             Console.WriteLine("Add in Github");
             Console.WriteLine("Github : make conflict");
-            console.WriteLine("Github 2nd try");
+            Console.WriteLine("Github 2nd try");
             Console.WriteLine("Add in Github");
             Console.WriteLine("Github : make conflict");
             Console.WriteLine("Local : make conflict");
